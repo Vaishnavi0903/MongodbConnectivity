389f7162bb24ac7408d8359e46355485f7db78b8
Vaishnavi Sontakke <129650128+Vaishnavi0903@users.noreply.github.com>
2023-11-17 22:36:52 +0530

    Add files via upload

---
+++ tut22.js
@@ -0,0 +1,23 @@
+// Refer mongodb.js 
+
+
+// Read data from Database
+
+const dbConnection = require ('./mongodb');
+
+// Handling Promises Method-I -----------------------------------
+// dbConnection().then((res) => {
+//     res.find({}).toArray().then((data) => {
+//         console.log(data);
+//     });
+// });
+
+
+// Handling Promise Method-II -------------------------------------
+const main = async () => {
+    let data = await dbConnection();
+    data = await data.find({}).toArray();
+    console.log(data);
+}
+
+main();
